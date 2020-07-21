29b5119e52673fa2d32359fe91cbb1946f12d648
Gabor Ottlik <gaborottlik@Gabors-MacBook-Pro.local>
2020-07-21 21:40:25 +0200

    Add error handling with ErrorBoundaries

--- src/container/Movies.jsx
+++ src/container/Movies.jsx
@@ -1,56 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Input, Row, Col, message } from 'antd'
 
 import { fetchMovies, resetMoviesError } from '../redux/reducers/movie'
 import MovieCard from '../component/movie-card/MovieCard'
 import './movies.scss'
+import { render } from '@testing-library/react'
 
 const { Search } = Input
 
-function Movies(props) {
-    const {
-        movies,
-        error,
-        fetchMovies,
-        resetMoviesError
-    } = props
-
-    function onCloseError() {
-        resetMoviesError()
+class Movies extends Component {
+    state = {
+        hasError: false
     }
 
-    return (
-        <>
-            { error && message.error(error, undefined, onCloseError) }
-            <div className='movies-container'>
-                <div className='search-bar'>
-                    <Search
-                        placeholder="input search text"
-                        enterButton="Search"
-                        size="large"
-                        onSearch={fetchMovies}
-                    />
+    onExpireError() {
+        this.setState({
+            hasError: false
+        })
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    render() {
+        const {
+            movies,
+            error,
+            fetchMovies,
+            resetMoviesError
+        } = this.props
+
+        const { hasError } = this.state
+
+        return (
+            <>
+                { error && message.error(error, undefined, resetMoviesError) }
+                { hasError && message.error('Something went wrong', undefined, this.onExpireError) }
+                <div className='movies-container'>
+                    <div className='search-bar'>
+                        <Search
+                            placeholder="input search text"
+                            enterButton="Search"
+                            size="large"
+                            onSearch={fetchMovies}
+                        />
+                    </div>
+                    <div className="result">
+                    <Row>
+                        <Col span={20} offset={2}>
+                            <Row gutter={16}>
+                                {movies.map(movie =>
+                                    <MovieCard
+                                        key={movie.id}
+                                        imageId={movie.poster_path}
+                                        title={movie.title}
+                                        description={movie.overview}
+                                    />
+                                )}
+                            </Row>
+                        </Col>
+                    </Row>
+                    </div>
                 </div>
-                <div className="result">
-                <Row>
-                    <Col span={20} offset={2}>
-                        <Row gutter={16}>
-                            {movies.map(movie =>
-                                <MovieCard
-                                    key={movie.id}
-                                    imageId={movie.poster_path}
-                                    title={movie.title}
-                                    description={movie.overview}
-                                />
-                            )}
-                        </Row>
-                    </Col>
-                </Row>
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
