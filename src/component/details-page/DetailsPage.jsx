c519b9d49524b393b73dbc7ba76d9692fdf1461e
Gabor Ottlik <gaborottlik@Gabors-MacBook-Pro.local>
2020-07-23 22:05:07 +0200

    Use skeleton as placeholder for content

--- src/component/details-page/DetailsPage.jsx
+++ src/component/details-page/DetailsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { LeftOutlined, CloseOutlined } from '@ant-design/icons'
-import { Typography } from 'antd';
+import { Typography, Skeleton } from 'antd';
 
 import './details-page.scss'
 
@@ -9,7 +9,27 @@ const { Title } = Typography;
 export default function DetailsPage(props) {
     const { onClose, details, img } = props
     const { original_title, release_date, vote_average, overview } = details
-    console.log('-- onClose', onClose)
+
+    function renderDetailsContentDescription() {
+        console.log('- details', details)
+        if (Object.keys(details).length) {
+            return (
+                <div className="details-content-description">
+                    <Title>{ original_title }</Title>
+                    <Title level={2}>{ release_date }</Title>
+                    <Title level={2}>{ vote_average }</Title>
+                    <div className="details-description">{ overview}</div>
+                </div>
+            )
+        }
+
+        return (
+            <div className="details-skeleton">
+                <Skeleton active />
+                <Skeleton active />
+            </div>
+        )
+    }
 
     return (
         <div className="detials-page-overlay">
@@ -24,12 +44,7 @@ export default function DetailsPage(props) {
                         </span>
                     </div>
                     <div className="details-content-wrapper">
-                        <div className="details-content-description">
-                            <Title>{ original_title }</Title>
-                            <Title level={2}>{ release_date }</Title>
-                            <Title level={2}>{ vote_average }</Title>
-                            <div className="details-description">{ overview}</div>
-                        </div>
+                        { renderDetailsContentDescription() }
                         <div className="details-image">
                             { img }
                         </div>
